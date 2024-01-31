e7ea597a3d1f9443caf48a8d4805c82085ba4bd3
borodinov_nikolai <borodinov.nikolai@gmail.com>
2024-01-31 20:42:54 +0300

    add back validation

--- back/src/users/dto/create-user.dto.ts
+++ back/src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
-    @ApiProperty({example: 'user@gmail.com', description: 'Почтовый адрес'})
-    readonly email: string;
-    @ApiProperty({example: '12345678', description: 'Пароль'})
-    readonly password : string;
+  @ApiProperty({ example: 'user@gmail.com', description: 'Почтовый адрес' })
+  @IsString({ message: 'Должно быть строкой' })
+  @IsEmail({}, { message: 'Неккоректный email' })
+  readonly email: string;
+  @ApiProperty({ example: '12345678', description: 'Пароль' })
+  @IsString({ message: 'Должно быть строкой' })
+  @Length(4, 16, { message: 'Не меньше 4 и не больше 16' })
+  readonly password: string;
 }
--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { User } from './users.model';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
+import { ValidationPipe } from 'src/pipes/validation.pipe';
 
 @ApiTags('Пользователи')
 @Controller('users')
@@ -12,15 +17,35 @@ export class UsersController {
     constructor(private usersService: UsersService) {}
     @ApiOperation({summary: 'Создание пользователя'})
     @ApiResponse({status: 200, type: User})
+    @UsePipes(ValidationPipe)
     @Post()
     create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
     @ApiOperation({summary: 'Получить всех пользователей'})
     @ApiResponse({status: 200, type: [User]})
-    @UseGuards(JwtAuthGuard)
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
     @Get()
     getAll() {
         return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto)
+    }
+
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status: 200})
+    @Roles("ADMIN")
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto)
     }
 }
