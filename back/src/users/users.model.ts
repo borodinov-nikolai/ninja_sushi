c82c70b37d059b091dc5c1b87c8bda955c2865e3
borodinov_nikolai <borodinov.nikolai@gmail.com>
2024-01-31 16:51:03 +0300

    add auth back

--- back/src/users/users.model.ts
+++ back/src/users/users.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { BelongsToMany, Column, DataType, Model, Table } from "sequelize-typescript";
+import { Role } from "src/roles/roles.model";
+import { UserRoles } from "src/roles/user-roles.module";
 
 
 interface UserCreationAttrs {
@@ -24,4 +26,6 @@ export class User extends Model<User, UserCreationAttrs> {
     @ApiProperty({example: 'За хулигантсво', description: 'Причина блокировки'})
     @Column({type: DataType.STRING, allowNull: true})
     banReason: string;
+    @BelongsToMany(()=>Role, ()=> UserRoles)
+    roles: Role[];
 }
